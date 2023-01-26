850087d7f94294d4f5ba93712efa7bcda66dc20e
Roger G Nascimento <roger.goulart.nascimento@gmail.com>
2023-01-26 20:03:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Component from "./component";
+
+const App = () => {
+  return <Component />;
+};
+
+export default App;
---
+++ src/Component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Component = () => {
+  return <p>Olá meu nome é Rafael</p>;
+};
+
+export default Component;
