aed177360db8a97635425df4319570457aa53afb
Roger G Nascimento <roger.goulart.nascimento@gmail.com>
2023-01-26 20:09:53 -0300

    desafio2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import { useState } from "react";
 import Component from "./component";
 
 const App = () => {
-  return <Component />;
+  const [contador, setContador] = useState(0);
+  return (
+    <>
+      <Component />
+      <p>Contador: {contador}</p>
+      <button onClick={() => setContador(contador + 1)}>Somar</button>
+    </>
+  );
 };
 
 export default App;
