e0ae9c5cdb5721c08c3183a8e7aea5472802d623
Roger G Nascimento <roger.goulart.nascimento@gmail.com>
2023-01-26 20:30:38 -0300

    desafio3

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import Component from "./component";
 
 const App = () => {
   const [contador, setContador] = useState(0);
+  const [nome, setNome] = useState("");
   return (
-    <>
+    <div className="container">
       <Component />
       <p>Contador: {contador}</p>
       <button onClick={() => setContador(contador + 1)}>Somar</button>
-    </>
+      <br />
+      <input
+        type="text"
+        placeholder="digite seu nome"
+        onChange={(e) => setNome(e.target.value)}
+      />
+      <p>Meu nome é: {nome}</p>
+    </div>
   );
 };
 
